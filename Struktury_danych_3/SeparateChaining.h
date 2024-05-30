64be7df14271e1dc921a4d046bab56cb36519850
kubikal7 <skubikal@gmail.com>
2024-05-30 15:07:53 +0200

    first commit, Separate Chaining

---
+++ Struktury_danych_3/SeparateChaining.h
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include "ArrayList.h"
+
+class SeparateChaining {
+private:
+    struct KeyValuePair {
+        int key;
+        int value;
+    };
+
+    ArrayList<KeyValuePair>* table;
+    int currentSize=100;
+    int numElements=0;
+    float loadFactorThreshold=0.7;
+
+    void rehash() {
+        int oldSize = currentSize;
+        currentSize *= 2; // New size is doubled
+        ArrayList<KeyValuePair>* newTable = new ArrayList<KeyValuePair>[currentSize];
+
+        for (int i = 0; i < oldSize; i++) {
+            int sizeBucket = table[i].getSize();
+            for (int j = 0;j < sizeBucket;j++) {
+                KeyValuePair pair = table[i].getElement(j);
+                int newIndex = hash(pair.key, currentSize);
+                newTable[newIndex].push_back(pair);
+            }
+        }
+
+        delete[] table;
+        table = newTable;
+    }
+
+    int hash(int key, int size) {
+        return key % size;
+    }
+
+public:
+    SeparateChaining(){
+        table = new ArrayList<KeyValuePair>[currentSize];
+    }
+
+    ~SeparateChaining() {
+        delete[] table;
+    }
+
+    void insert(int key, int value) {
+        float currentLoadFactor = static_cast<float>(numElements) / currentSize;
+        if (currentLoadFactor > loadFactorThreshold) {
+            rehash();
+        }
+        int index = hash(key, currentSize);
+        table[index].push_back({ key, value });
+        numElements++;
+    }
+
+    int find(int key) {
+        int index = hash(key, currentSize);
+        int sizeBucket = table[index].getSize();
+        for (int j = 0;j < sizeBucket;j++) {
+            KeyValuePair pair = table[index].getElement(j);
+            if (pair.key == key) {
+                return pair.value;
+            }
+        }
+        // Return default value if key not found
+        return -1;
+    }
+
+    void remove(int key) {
+        int index = hash(key, currentSize);
+        int sizeBucket = table[index].getSize();
+        for (int j = 0;j < sizeBucket;j++) {
+            KeyValuePair pair = table[index].getElement(j);
+            if (pair.key == key) {
+                table[index].deleteElement(j);
+                numElements--;
+                break; // Stop after first occurrence of key
+            }
+        }
+    }
+
+
+
+    void printTable() {
+        for (int i = 0; i < currentSize; ++i) {
+            int sizeBucket = table[i].getSize();
+            std::cout << "Bucket " << i << ": ";
+            for (int j = 0;j < sizeBucket;j++) {
+                KeyValuePair pair = table[i].getElement(j);
+                std::cout << "(" << pair.key << ", " << pair.value << ") -> ";
+            }
+            std::cout << "NULL" << std::endl;
+        }
+    }
+};
+
+
---
+++ Struktury_danych_3/Struktury_danych_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "SeparateChaining.h"
+
+using namespace std;
+
+int main()
+{
+    SeparateChaining ht;
+    for (int i = 0;i < 72;i++) {
+        ht.insert(i, i + 100);
+    }
+
+    ht.printTable();
+    cout << ht.find(103) << endl;
+    /*std::cout << "Value for key 2: " << ht.find(2) << std::endl;
+    std::cout << "Value for key 4: " << ht.find(4) << std::endl;
+
+    ht.remove(2);
+
+    std::cout << "Table after deletion:" << std::endl;
+    ht.printTable();*/
+
+    return 0;
+}
+
